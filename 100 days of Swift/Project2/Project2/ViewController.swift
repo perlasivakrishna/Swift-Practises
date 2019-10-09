aa18ea140e5ee538aced244dec6d30a7f75e4c25
siva krishna <sivakrishna.perla@ymail.com>
2019-10-09 17:59:59 +0530

    SwiftUI Day 16
    WeSplit Project

---
+++ 100 days of Swift/Project2/Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by siva krishna on 04/10/19.
+//  Copyright © 2019 Siva Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
